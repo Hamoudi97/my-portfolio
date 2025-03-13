297bfa820f7c03bbc29975a553bdb362f88420f2
Ahmed <hamoudi1997@yahoo.com>
2025-03-13 09:42:31 -0400

    Typed.js implemented + minor adjustments

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     projectToggle();
     formValidation();
     liveFormValidation();
+    new Typed("#typed-text", {
+        strings: ["a Student.", "a Gamer.", "a Foodie.", "a Web Developer."],
+        typeSpeed: 40,
+        showCursor: false,
+        startDelay: 250,
+    })
 })
 
 function projectToggle() {
@@ -46,7 +52,7 @@ function formValidation() {
         if (emailInput.value.trim() === "") {
             emailError.textContent = "Please enter a valid email address.";
             isValid = false;
-        } else if (!validateEmail(emailInput.value.trim())){
+        } else if (!validateEmail(emailInput.value.trim())) {
             emailError.textContent = "Invalid email format."
             isValid = false;
         }
@@ -58,7 +64,7 @@ function formValidation() {
 
         if (isValid) {
             submitMessage.textContent = "Your message has been sent! ✅"
-            setTimeout(()=> {
+            setTimeout(() => {
                 form.reset();
                 submitMessage.textContent = "";
             }, 7000);
@@ -68,13 +74,13 @@ function formValidation() {
     })
 }
 
-function validateEmail(email){
+function validateEmail(email) {
     const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return re.test(String(email));
 }
 
 
-function liveFormValidation(){
+function liveFormValidation() {
     nameInput.addEventListener("input", () => nameError.textContent = "")
     emailInput.addEventListener("input", () => emailError.textContent = "")
     messageInput.addEventListener("input", () => messageError.textContent = "")
